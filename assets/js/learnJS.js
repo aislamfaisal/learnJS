466b0618000629c6007564bcb5ce99866b0fcde6
Ariful Islam <aislamfaisal@gmail.com>
2017-11-02 17:17:50 +0600

    Show result in another div

--- assets/js/learnJS.js
+++ assets/js/learnJS.js
@@ -3,3 +3,9 @@
 function alertMessage(){
     alert("My\tname\tis\nFaisal");
 }*/
+
+/*Show result in another div*/
+function buttonMessage(text){
+    var textUpper = text.toUpperCase();
+    document.getElementById("showText").innerHTML = textUpper;
+}
