975e8d7ba6eb67d01d29d19cfbfe1ee4711df12b
Ariful Islam <aislamfaisal@gmail.com>
2017-11-02 18:08:53 +0600

    Show string length

--- assets/js/learnJS.js
+++ assets/js/learnJS.js
@@ -5,7 +5,17 @@ function alertMessage(){
 }*/
 
 /*Show result in another div*/
+/*
 function buttonMessage(text){
     var textUpper = text.toUpperCase();
     document.getElementById("showText").innerHTML = textUpper;
 }
+*/
+
+/*
+String length*/
+/*function showStringLength(){
+    var string = document.stringLength.stringInput.value;
+    var charLength = string.length;
+    document.getElementById("showLength").innerHTML = charLength;
+}*/
