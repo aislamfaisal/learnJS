231201a6d883706aa18df504f938a60c422d4461
aislamfaisal <aislamfaisal@gmail.com>
2017-11-15 02:57:23 +0600

    Testing JS

--- assets/js/learnJS.js
+++ assets/js/learnJS.js
@@ -21,11 +21,33 @@ String length*/
 }*/
 
 /*Print value from form*/
-function showEmployeeInfo(){
+/*function showEmployeeInfo(){
     var name = document.employeeInfo.employeeName.value;
     var email = document.employeeInfo.employeeEmail.value;
     var phone = document.employeeInfo.employeePhone.value;
     var education = document.employeeInfo.employeeEducation.value;
     var showInfo = "Name: " + name + "<br>" + "Email: " +  email + "<br>" + "Phone: " +  phone + "<br>" + "Education: " + education;
     document.getElementById("employeeInfoShow").innerHTML = showInfo;
+}*/
+
+
+/*Temperature Convert*/
+/*function tempResult(){
+    var temperature = Number(document.showTemperature.inputTemp.value);
+    var tempType = document.showTemperature.choice.value;
+    var result;
+
+    if(tempType == 'fahrenheit'){
+        result = 9/5 * temperature + 32;
+    }
+    else{
+        result = 5/9 * (temperature - 32);
+    }
+    document.getElementById("showResult").innerHTML = result;
+}*/
+
+function allListItem(){
+    var listsItem = document.checkList.listItem.value;
+    var showLists = '<ul class="list-group"><li class="list-group-item"><input type="checkbox" checked>' + listsItem + '</li></ul>';
+    document.getElementById('showList').innerHTML += showLists;
 }
